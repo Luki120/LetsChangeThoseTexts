1ab8a2227e757e21f4542122e176717572269577
Luki120 <lukicassino@hotmail.com>
2021-07-08 05:46:38 -0300

    .

--- lcttprefs/LCTTRootListController.h
+++ lcttprefs/LCTTRootListController.h
@@ -1,5 +1,8 @@
 #import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSTableCell.h>
 
 
 @interface LCTTRootListController : PSListController
+@property (nonatomic, retain) UIBarButtonItem *killButton;
 @end
