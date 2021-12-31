905e425102050b52263658506d45f533aaf7cc29
luki120 <luki120@exerra.xyz>
2021-12-31 02:00:09 -0300

    Constants file

---
+++ Headers/Constants.h
@@ -0,0 +1,8 @@
+@import UIKit;
+
+
+static NSString *const kIgPath = @"/var/mobile/Library/Preferences/me.luki.runtimeoverflow.lcttinstagram.plist";
+static NSString *const kTwitterPath = @"/var/mobile/Library/Preferences/me.luki.runtimeoverflow.lctttwitter.plist";
+
+#define kIgTintColor [UIColor colorWithRed: 0.84 green: 0.16 blue: 0.46 alpha: 1.0]
+#define kTwitterTintColor [UIColor colorWithRed: 0.0 green: 0.67 blue: 0.93 alpha: 1.0]
